595e05f0cc19a4ea09f6604043ea4820658ff013
ookkdai <78086517+ookkdai@users.noreply.github.com>
2021-04-19 13:00:44 +0900

    データ投入用のファイルのコードを記述

--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -1,3 +1,24 @@
 require 'csv'
 
 
+class ImportCsv
+
+
+  def self.import(path)
+    list = []
+
+    CSV.foreach(path, headers: true) do |row|
+      list << row.to_h
+    end
+    list
+  end
+
+  def self.text_data
+    list = import('db/csv_data/text_data.csv')
+    Text.delete
+
+    puts "テキスト教材インポート処理を開始"
+    Text.create!(list)
+    puts "テキスト教材のCSVデータインポート完了!!"
+  end
+
