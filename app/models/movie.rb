107d852a0e72c11441c5d48408b7fc3315336d82
nuntanunta5153 <nuntanunta5153@gmail.com>
2021-04-20 03:11:14 +0900

    add model to validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,24 @@
 class Movie < ApplicationRecord
-    
+    validates :genre, presence: true
+    validates :title, presence: true
+    validates :url, presence: true
+
+    enum genre: {
+        invisible: 0, 
+        basic: 1,
+        git: 2,
+        ruby: 3,
+        rails: 4,
+        php: 5,
+        html: 6,
+        javascript: 7,
+        typescript: 8,
+        react: 9,
+        vue: 10,
+        angular: 11,
+        aws: 12,
+        money: 13, 
+        talk: 14, 
+        live: 15, 
+      }
 end
