389c2ddd26dae44063daceda456a5c347f40c32c
shutheonetokyo <ss1030ss@icloud.com>
2021-04-28 15:07:51 +0900

    db/seeds.rb をタスク6と同様の形式（何度実行してもエラーが出ない形式）に修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@ User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
+
+Text.delete_all
+ImportCsv.text_data
+Movie.delete_all
+ImportCsv.movie_dataAdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
