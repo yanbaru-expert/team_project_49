242c546b4cd81d578c89ab14ad524c642357e59d
shutheonetokyo <ss1030ss@icloud.com>
2021-05-01 00:20:50 +0900

    find_or_create_byの箇所を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ end
 ADMIN_EMAIL = 'admin@example.com'
 ADMIN_PASSWORD = 'password'
 
-AdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') do |admin_user|
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
   admin_user.password = ADMIN_PASSWORD
-  puts 'ユーザーの初期データインポートに成功しました。'
+  puts '管理者ユーザーの初期データインポートに成功しました。'
 end
 
 Text.delete_all
