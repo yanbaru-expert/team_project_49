cc57ebfaf3cb3e066c48ffcf2136730623538599
nuntanunta5153 <nuntanunta5153@gmail.com>
2021-04-30 16:37:30 +0900

    テキスト教材一覧の追加

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,4 +1,11 @@
 class TextsController < ApplicationController
   def index
+    if params[:genre] == nil
+      @texts = Text.where(genre: ["Basic", "Git", "Ruby", "Ruby on Rails"])
+    else
+      @texts = Text.where(genre: params[:genre])
+    end
+
   end
+
 end
