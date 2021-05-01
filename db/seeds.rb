a2d928d00adb789d14c67749c573d8612cbaedb9
shutheonetokyo <ss1030ss@icloud.com>
2021-05-01 20:41:04 +0900

    定数の移動の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
+ADMIN_EMAIL = 'admin@example.com'
+ADMIN_PASSWORD = 'password'
 
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
-
-ADMIN_EMAIL = 'admin@example.com'
-ADMIN_PASSWORD = 'password'
 
 AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
   admin_user.password = ADMIN_PASSWORD
