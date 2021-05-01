c1b40b147e51b33e26638b3ffc567be0c46028a5
nuntanunta5153 <nuntanunta5153@gmail.com>
2021-05-01 21:32:27 +0900

    テキスト教材一覧ページの追加3

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,9 +1,7 @@
 class TextsController < ApplicationController
   def index
-    if params[:genre].in?(genre: params[5..15])
-      @texts = Text.where(genre: params[:genre])
-    else
-      @texts = Text.where(genre: ["Basic", "Git", "Ruby", "Ruby on Rails"])
-    end
+    @texts = Text.where(genre: ["Basic", "Git", "Ruby", "Ruby on Rails"])
   end
 end
+
+
