a0c08f6be68314f13120d7a536cd11ce76d5ed73
nuntanunta5153 <nuntanunta5153@gmail.com>
2021-05-01 21:01:30 +0900

    テキスト教材一覧ページの追加を修正2

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,6 @@
 class TextsController < ApplicationController
   def index
-    if params[:genre].in?(genre: ["Basic", "Git", "Ruby", "Ruby on Rails"])
+    if params[:genre].in?(genre: params[5..15])
       @texts = Text.where(genre: params[:genre])
     else
       @texts = Text.where(genre: ["Basic", "Git", "Ruby", "Ruby on Rails"])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'texts#index'
-  resources :texts
+  resources :texts, only: [:index, :show]
 end
