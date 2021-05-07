2f00b45d9e7d7c6858c2bd2587070679109131c0
shutheonetokyo <ss1030ss@icloud.com>
2021-05-07 23:00:51 +0900

    Ruby/Rails 動画教材 の配置の修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,11 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc)
+    @movies = Movie.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
   end
 
   def show
-    @movie = .find(params[:id])
+    @movie = Movie.find(params[:id])
   end
 
 end
